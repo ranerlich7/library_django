f99a7d92fb6f99a2a6335b6ecd1c130824c2e15e
ran <ran@goz.com>
2022-11-02 18:29:23 +0200

    added book form

--- books/forms.py
+++ books/forms.py
@@ -1,8 +1,11 @@
 from django.forms import ModelForm
 from .models import Book
+from django.contrib.auth.models import User
+
  
 class BookForm(ModelForm):
     class Meta:
         model = Book
         fields = ['name', 'author', 'year_published', 'type', 'image']
  
+        
