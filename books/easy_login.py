b860a05e4c482c0d11064243e7a3ad7775273a21
ran <ran@goz.com>
2022-11-05 22:07:05 +0200

    login and logout implemented

--- books/easy_login.py
+++ books/easy_login.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 def elogin(request):
     if request.POST:
-        return redirect('books:books')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'user does not exist')
+            return redirect('elogin')
+        
+        authuser = authenticate(request,username=username, password=password)
+        if authuser:
+            login(request, authuser)
+            return redirect('books:books')
+        else:
+            messages.error(request, 'Incorrect password')
+            return redirect('elogin')
+
     else:
         context = {}
         return render(request,'elogin.html',context=context)
+
+def mylogout(request):
+    logout(request)
+    context = {}
+    return redirect('books:books')
+    
